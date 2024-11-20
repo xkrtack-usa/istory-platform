ea6f781326059ce068c78a6c3e06b533bab5866f
dangtong76 <dangtong@gmail.com>
2024-11-20 15:53:56 +0900

    add network.tf

--- variables.tf
+++ variables.tf
@@ -1,33 +1,33 @@
-# variable "aws_access_key_id" {
-#   description = "AWS Access Key ID"
-#   type        = string
-# }
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
 
-# variable "aws_secret_access_key" {
-#   description = "AWS Secret Access Key"
-#   type        = string
-# }
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
 
-# variable "aws_region" {
-#   description = "AWS Region"
-#   type        = string
-#   default     = "ap-northeast-2"  # 기본값 설정 (선택사항)
-# }
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-northeast-2"  # 기본값 설정 (선택사항)
+}
 
-# variable "vpc_name" {
-#   description = "name of vpc"
-#   type        = string
-#   default     = "dangtong"
-# }
+variable "vpc_name" {
+  description = "name of vpc"
+  type        = string
+  default     = "dangtong"
+}
 
-# variable "cluster_name" {
-#   description = "name of cluster"
-#   type        = string
-#   default     = "istory"
-# }
+variable "cluster_name" {
+  description = "name of cluster"
+  type        = string
+  default     = "istory"
+}
 
-# variable "environment" {
-#   description = "Environment name (e.g., dev, stg, prd)"
-#   type        = string
-#   default     = "dev"  # 필요한 경우 기본값 설정
-# }
+variable "environment" {
+  description = "Environment name (e.g., dev, stg, prd)"
+  type        = string
+  default     = "dev"  # 필요한 경우 기본값 설정
+}
