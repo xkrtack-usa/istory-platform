d426da1e793fff2a79eeb112187078cb2a1a6b91
dangtong76 <dangtong@gmail.com>
2024-11-20 14:45:32 +0900

    add network.tf

--- 000_provider.tf
+++ 000_provider.tf
@@ -1,10 +1,6 @@
-#########################################################################################################
-## Configure the AWS Provider
-#########################################################################################################
 provider "aws" {
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
-#  token      = var.aws_session_token
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 
   profile = var.terraform_aws_profile
   region = var.aws_region
