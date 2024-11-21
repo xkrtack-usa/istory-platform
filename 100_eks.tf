bb3ddd472f22f59e39adeff29faa852e7e0b90d9
dangtong76 <dangtong@gmail.com>
2024-11-21 12:29:33 +0900

    add eks

--- 100_eks.tf
+++ 100_eks.tf
@@ -3,7 +3,7 @@
 #########################################################################################################
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 19.0"
+  version         = "~> 20.29.0"
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
